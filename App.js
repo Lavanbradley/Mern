1bae1b84a103f0e9808d44fb8c06435b8d6b4276
Lavanbradley <79238572+Lavanbradley@users.noreply.github.com>
2021-09-26 22:32:16 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {BrowserRouter, Route} from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+import Login from './pages/Login'
+import Register from './pages/Register'
+const App = () => {
+  return <div>
+    <BrowserRouter>
+    <Route path="/login" exact component={Login}/>
+    <Route path="/register" exact component={Register}/>
+    <Route path="/dashboard" exact component={Dashboard}/>
+    </BrowserRouter>
+  </div>
+}
+
+export default App;
